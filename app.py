133da316e7c63541758fe55ce5fa42e3a18e227b
DiegxKing <jazaneropq1@upao.edu.pe>
2025-06-11 14:45:31 -0500

    app.py

--- app.py
+++ app.py
@@ -38,5 +38,8 @@ class Result(Resource):
 
 api.add_resource(Result, '/result')
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
